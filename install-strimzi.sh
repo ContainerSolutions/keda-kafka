61ea89d5d5b82e0254cf70da3869cf037002c5d6
Serdar Kalaycı <serdarkalayci@gmail.com>
2023-09-11 14:12:12 +0200

    Minor twaeks for a smoother demo

--- install-strimzi.sh
+++ install-strimzi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 kubectl create namespace kafka
 kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
-kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml -n kafka
+kubectl apply -f kafka-cluster-ephemeral.yaml -n kafka
 kubectl wait kafka/my-cluster --for=condition=Ready --timeout=300s -n kafka
